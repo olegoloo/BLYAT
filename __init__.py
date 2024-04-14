45649f15929fec59de6c0f88bcde09b6d97f0bfa
Лиза <104992063+olegoloo@users.noreply.github.com>
2024-04-15 01:42:35 +0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,2 +1,13 @@
-__version__ = '5.10.4'
-# don't import anything else here because setup.py imports this.
+from typing import List, Optional
+
+__version__ = "23.2.1"
+
+
+def main(args: Optional[List[str]] = None) -> int:
+    """This is an internal API only meant for use by pip's own console scripts.
+
+    For additional details, see https://github.com/pypa/pip/issues/7498.
+    """
+    from pip._internal.utils.entrypoints import _wrapper
+
+    return _wrapper(args)
